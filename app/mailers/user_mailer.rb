cf3382040a655d116c42b980da0e6072b7504557
jimikant <jimiv.essence@gmail.com>
2024-08-29 18:14:41 +0530

    Sending mail when user created

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email
     @user = params[:user]
-    @url  = 'http://localhost:3000'
+    @url  = 'http://localhost:3000/users/password/new'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
