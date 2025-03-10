427b9d920dbc579b93e2b96679e0f9c1a85d78d5
jimikant <jimiv.essence@gmail.com>
2025-03-10 20:27:50 +0530

    remove some migration which cause conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_30_125405) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_30_125221) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -133,13 +133,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_30_125405) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "username"
     t.integer "role", default: 0, null: false
     t.string "slug"
     t.string "stripe_customer_id"
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
